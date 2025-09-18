a86a99a639ee99fa2849b0fde6d09232394b8d05
joaquin002 <joaquin.manuel.olivera123@gmail.com>
2025-09-18 15:17:03 -0300

    clase pasajeros y enum categoria

---
+++ src/VehiculoPasajeros.java
@@ -0,0 +1,21 @@
+public class VehiculoPasajeros extends Vehiculo {
+    private int CantPasajeros
+    private CategoriaDelVehiculo Categoria;
+
+    public VehiculoPasajeros(Marcas marca, String modelo, int patente, int velRecomendada, int kilometraje, int cantPasajeros, CategoriaDelVehiculo categoria) {
+        super(marca, modelo, patente, velRecomendada, kilometraje);
+        CantPasajeros = cantPasajeros;
+        Categoria = categoria;
+    }
+
+    public int getCantPasajeros() {
+        return CantPasajeros;
+    }
+
+    public CategoriaDelVehiculo getCategoria() {
+        return Categoria;
+    }
+}
+/*Los vehículos que transportan pasajeros cuentan con una cantidad de pasajeros que pueden
+llevar,la categoría del vehículo que puede ser: standard, premium y deluxe
+ */
